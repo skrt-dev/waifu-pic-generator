74124781490f1a13c21d89dfb972a7a1269a45fe
skrt-dev <skrt.developer@gmail.com>
2021-08-28 17:51:43 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       </Head>
       <div className="top-1/2 absolute flex justify-center flex-col">
         <h1 className="dark:text-white text-gray-800 text-3xl font-bold text-center transition-colors">Click me</h1>
-        <button onClick={() => { setLoad(true); getPic().then(response => window.open(response)) }} className={"text-white  text-zl rounded-md py-1 mt-2  bg-pink-400 hover:bg-pink-500 inline-flex justify-center items-center"}>{load ?
+        <button onClick={() => { setLoad(true); window.open(getPic())} } className={"text-white  text-zl rounded-md py-1 mt-2  bg-pink-400 hover:bg-pink-500 inline-flex justify-center items-center"}>{load ?
           <svg className="animate-spin -ml-1 mr-2 h-5 w-5 text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
