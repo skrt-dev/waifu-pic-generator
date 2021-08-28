1560e04e0b5680de7c30aea5da3cff2f081ed445
skrt-dev <skrt.developer@gmail.com>
2021-08-28 18:05:59 -0500

    fix mobile

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,18 @@ import Head from 'next/head';
 export default function Home() {
   const [load, setLoad] = useState(false);
   const [dark, setDark] = useState(true);
+  const [url, setUrl] = useState('');
+  const [ready, setReady] = useState(false);
   const category = ['waifu', 'neko', 'shinobu', 'megumin']
   const getPic = async () => {
+    setLoad(true)
     let categoryIndex = Math.floor(Math.random() * category.length);
-    return fetch(`https://api.waifu.pics/sfw/${category[categoryIndex]}`)
+    await fetch(`https://api.waifu.pics/sfw/${category[categoryIndex]}`)
       .then(response => response.json())
-      .then((data) => { setLoad(false); return data.url })
+      .then((data) => { setLoad(false); setUrl(data.url); setReady(true) })
       .catch(error => alert(error))
   }
+  const openPic = () => {window.open(url); setReady(false)};
   return (
 <div className={dark ? "dark" : null}>
     <div className="h-screen w-screen dark:bg-black bg-gray-200 relative flex justify-center transition-colors">
@@ -20,12 +24,12 @@ export default function Home() {
       </Head>
       <div className="top-1/2 absolute flex justify-center flex-col">
         <h1 className="dark:text-white text-gray-800 text-3xl font-bold text-center transition-colors">Click me</h1>
-        <button onClick={() => { setLoad(true); window.open(getPic())} } className={"text-white  text-zl rounded-md py-1 mt-2  bg-pink-400 hover:bg-pink-500 inline-flex justify-center items-center"}>{load ?
+        <button onClick= {() => { ready ? openPic() : getPic()} } className={"text-white  text-zl rounded-md py-1 mt-2  bg-pink-400 hover:bg-pink-500 inline-flex justify-center items-center"}>{load ?
           <svg className="animate-spin -ml-1 mr-2 h-5 w-5 text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
           </svg> : null}
-          {load ? "Processing" : 'Generate'}
+          {load ? "Processing" : ready ? 'Open' : 'Generate'}
         </button>
       </div>
       <a href className="absolute right-5 bottom-5 dark:text-white text-gray-800 transition-colors" target="_blank" rel="noreferrer" href="https://github.com/skrt-dev/waifu-pic-generator">
